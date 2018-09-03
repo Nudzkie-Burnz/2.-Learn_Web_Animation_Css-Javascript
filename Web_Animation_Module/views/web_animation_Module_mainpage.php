05f4a95c1828f49d5be0e5374ec5a724cfab0487
stan <sbnudo@village88.com>
2018-09-03 16:20:04 +0800

    added web animation php files

---
+++ Web_Animation_Module/views/web_animation_Module_mainpage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Web Animation Module</title>
+</head>
+<body>
+	<h1>Web Animation Module</h1>
+	<hr>
+	<div id="web_animation_links_container">
+		<a href="views\css_animation1.php" id="link1" class="links">Css Animation</a>
+		<a href="css_transitions2.php" id="link2" class="links">Css Transition</a>
+		<a href="Web_Animation_Module\views\" id="link3" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link4" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link5" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link6" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link7" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link8" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link9" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link10" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link11" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link12" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link13" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link14" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link15" class="links"></a>
+		<a href="Web_Animation_Module\views\" id="link16" class="links"></a>
+	</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Web Animation Module</title>
+	<style>
+		*{
+			font-family: arial, sans-serif;
+			margin: 0px;
+			padding: 0px;
+			text-align: center;
+		}
+		hr{
+			margin: 10px auto;
+			width: 600px;
+		}
+		h1{
+			margin-top: 20px;
+		}
+		div#web_animation_links_container{
+			margin: 30px auto;
+			padding: 5px;
+			text-align: justify;
+			width: 600px;
+		}
+		div#web_animation_links_container a{
+			color: #70e584;
+			font-weight: bold;
+			margin-left: 5px;
+			text-decoration: none;
+		}
+		div#web_animation_links_container a:visited{
+			color: #76b080;
+		}
+		div#web_animation_links_container a:hover{
+			color: #507055;
+			text-decoration: underline;
+		}
+		div#web_animation_links_container a:after{
+			content: '/';
+			color: #507055;
+			margin-left: 2px;
+		}
+		div#web_animation_links_container a:last-child:after{
+			content: '';
+		}
+	</style>
+</head>
+<body>
+	<h1>Web Animation Module</h1>
+	<hr>
+	<div id="web_animation_links_container">
+		<a href="Web_Animation_Module\views\css_animation1.php" id="link1" class="links">Css Animation</a>
+		<a href="Web_Animation_Module\views\css_transitions2.php" id="link2" class="links">Css Transition</a>
+		<a href="Web_Animation_Module\views\css_easing3.php" id="link3" class="links">Css Easing Animation</a>
+		<a href="Web_Animation_Module\views\css_animation_events4.php" id="link4" class="links">Css Animation Events/Js</a>
+		<a href="Web_Animation_Module\views\css_js_animation_frame5.php" id="link5" class="links">Css Animation Frame/Js</a>
+		<a href="Web_Animation_Module\views\css_smooth_animation6.php" id="link6" class="links">Css Smooth Animation</a>
+		<a href="Web_Animation_Module\views\css_hover_image7.php" id="link7" class="links">Css Hover Animation</a>
+		<a href="Web_Animation_Module\views\css_transition_event_on_javascript8.php" id="link8" class="links">Css Transition Event On Js</a>
+		<a href="Web_Animation_Module\views\css_kenburns_effect9.php" id="link9" class="links">Css The Ken Burn's Effect</a>
+		<a href="Web_Animation_Module\views\css_jittery_effect10.php" id="link10" class="links">Css Jittery Effect</a>
+		<a href="Web_Animation_Module\views\css_recreating_the_blink_tag11.php" id="link11" class="links">Css Recreating the Blink Tag</a>
+		<a href="Web_Animation_Module\views\css_moveElement_on_click12.php" id="link12" class="links">Css Move Element on Click</a>
+		<a href="Web_Animation_Module\views\css_smooth_sliding_menu13.php" id="link13" class="links">Css Smooth Sliding</a>
+		<a href="Web_Animation_Module\views\css_animating_links_to_life14.php" id="link14" class="links">Css Animating Links</a>
+		<a href="Web_Animation_Module\views\css_smooth_parallaxing15.php" id="link15" class="links">Css Smooth Parallaxing</a>
+		<a href="Web_Animation_Module\views\css_faking_a_morph_animation16.php" id="link16" class="links">Css Faking a Morph Animation</a>
+	</div>
+</body>
+</html>
